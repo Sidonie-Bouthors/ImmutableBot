ab20a6d7d3e25694d6985c5721e4b0d716cc6d3f
SidonieBouthors <sidonie.bouthors@epfl.ch>
2022-09-03 16:55:03 +0200

    commit 1

--- bot.py
+++ bot.py
@@ -18,15 +18,26 @@ logging.basicConfig(format = "%(asctime)s - %(name)s - %(levelname)s  - %(messag
 
 logger = logging.getLogger(__name__)
 
+helpfulMood = false
+
 def start(update, context): 
   """reply when called upon for the first time"""
-  update.message.reply_text("Who dare calls upon me")
+  update.message.reply_text("Who dares call upon me")
 
 def help(update, context):
-  """do not offer help when help is requested"""
-  update.message.reply_text(
-    """You are not deserving of my help
-    """, parse_mode="HTML")
+  """offer help when help is requested? maybe"""
+  if (helfulMood) :
+    update.message.reply_text(
+      """Fine, I shall help you
+      """, parse_mode="HTML")
+  else :
+    update.message.reply_text(
+      """You are not deserving of my help
+      """, parse_mode="HTML")
+
+def sus(update, context):
+  """suspicious"""
+  update.message.reply_text("Suspicious activity detected. Beware!")
 
 def trial(update, context): 
   """trial time"""
@@ -34,7 +45,7 @@ def trial(update, context):
 
 def error(update, context): 
   """error"""
-  logger.error(f"Something is broken : {context.error}")
+  logger.error(f"You broke something, fix it : {context.error}")
 
 def main(): 
   # start the bot
@@ -44,6 +55,14 @@ def main():
 
   dp.add_handler(CommandHandler("start", start))
   dp.add_handler(CommandHandler("help", help))
+
+  dp.add_handler(MessageHandler(Filters.regex("caffeine")
+				| Filters.regex("coffee")
+				| Filters.regex("café")
+				| Filters.regex("caféine")
+				| Filters.regex("maté")
+				| Filters.regex("secte")
+				| Filters.regex("culte"), sus))
   
   dp.add_error_handler(error)
 

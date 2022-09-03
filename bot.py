3766d1071e0281a193675cc8bef3d88ff5a60031
SidonieBouthors <sidonie.bouthors@epfl.ch>
2022-09-03 19:00:42 +0200

    commit more

--- bot.py
+++ bot.py
@@ -66,8 +66,6 @@ def main():
   
   dp.add_error_handler(error)
 
-  # updater.start_polling()
-
   updater.start_webhook(listen = '0.0.0.0', 
                         port = int(PORT), 
                         url_path = TOKEN)
